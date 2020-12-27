6144300a8b4074c3b2976eb225c152a3e92055b1
ramosv <56368263+ramosv@users.noreply.github.com>
2020-12-26 17:53:24 -0700

    add

--- BrightenTheBrainApp/OAuthNativeFlowPage.xaml.cs
+++ BrightenTheBrainApp/OAuthNativeFlowPage.xaml.cs
@@ -113,73 +113,43 @@ namespace BrightenTheBrainApp
 
             if (e.IsAuthenticated)
             {
-                if (authenticator.AuthorizeUrl.Host == "www.facebook.com")
+                User user = null;
+
+                // If the user is authenticated, request their basic user data from Google
+                // UserInfoUrl = https://www.googleapis.com/oauth2/v2/userinfo
+                var request = new OAuth2Request("GET", new Uri(Constants.GoogleUserInfoUrl), null, e.Account);
+                var response = await request.GetResponseAsync();
+                if (response != null)
                 {
-                    //FacebookEmail facebookEmail = null;
-
-                    var httpClient = new HttpClient();
-
-                    var json = await httpClient.GetStringAsync($"https://graph.facebook.com/me?fields=id,name,first_name,last_name,email,picture.type(large)&access_token=" + e.Account.Properties["access_token"]);
-
-                    //facebookEmail = JsonConvert.DeserializeObject<FacebookEmail>(json);
-
-                    await store.SaveAsync(account = e.Account, Constants.AppName);
-
-                    Application.Current.Properties.Remove("Id");
-                    Application.Current.Properties.Remove("FirstName");
-                    Application.Current.Properties.Remove("LastName");
-                    Application.Current.Properties.Remove("DisplayName");
-                    Application.Current.Properties.Remove("EmailAddress");
-                    Application.Current.Properties.Remove("ProfilePicture");
-
-                    //Application.Current.Properties.Add("Id", facebookEmail.Id);
-                    //Application.Current.Properties.Add("FirstName", facebookEmail.First_Name);
-                    //Application.Current.Properties.Add("LastName", facebookEmail.Last_Name);
-                    //Application.Current.Properties.Add("DisplayName", facebookEmail.Name);
-                    //Application.Current.Properties.Add("EmailAddress", facebookEmail.Email);
-                    //Application.Current.Properties.Add("ProfilePicture", facebookEmail.Picture.Data.Url);
-
-                    //await Navigation.PushAsync(new ProfilePage());
+                    // Deserialize the data and store it in the account store
+                    // The users email address will be used to identify data in SimpleDB
+                    string userJson = await response.GetResponseTextAsync();
+                    user = JsonConvert.DeserializeObject<User>(userJson);
                 }
-                else
+
+                if (account != null)
                 {
-                    User user = null;
-
-                    // If the user is authenticated, request their basic user data from Google
-                    // UserInfoUrl = https://www.googleapis.com/oauth2/v2/userinfo
-                    var request = new OAuth2Request("GET", new Uri(Constants.GoogleUserInfoUrl), null, e.Account);
-                    var response = await request.GetResponseAsync();
-                    if (response != null)
-                    {
-                        // Deserialize the data and store it in the account store
-                        // The users email address will be used to identify data in SimpleDB
-                        string userJson = await response.GetResponseTextAsync();
-                        user = JsonConvert.DeserializeObject<User>(userJson);
-                    }
-
-                    if (account != null)
-                    {
-                        store.Delete(account, Constants.AppName);
-                    }
-
-                    await store.SaveAsync(account = e.Account, Constants.AppName);
-
-                    Application.Current.Properties.Remove("Id");
-                    Application.Current.Properties.Remove("FirstName");
-                    Application.Current.Properties.Remove("LastName");
-                    Application.Current.Properties.Remove("DisplayName");
-                    Application.Current.Properties.Remove("EmailAddress");
-                    Application.Current.Properties.Remove("ProfilePicture");
-
-                    Application.Current.Properties.Add("Id", user.Id);
-                    Application.Current.Properties.Add("FirstName", user.GivenName);
-                    Application.Current.Properties.Add("LastName", user.FamilyName);
-                    Application.Current.Properties.Add("DisplayName", user.Name);
-                    Application.Current.Properties.Add("EmailAddress", user.Email);
-                    Application.Current.Properties.Add("ProfilePicture", user.Picture);
-
-                    await Navigation.PushAsync(new ProfilePage());
+                    store.Delete(account, Constants.AppName);
                 }
+
+                await store.SaveAsync(account = e.Account, Constants.AppName);
+
+                Application.Current.Properties.Remove("Id");
+                Application.Current.Properties.Remove("FirstName");
+                Application.Current.Properties.Remove("LastName");
+                Application.Current.Properties.Remove("DisplayName");
+                Application.Current.Properties.Remove("EmailAddress");
+                Application.Current.Properties.Remove("ProfilePicture");
+
+                Application.Current.Properties.Add("Id", user.Id);
+                Application.Current.Properties.Add("FirstName", user.GivenName);
+                Application.Current.Properties.Add("LastName", user.FamilyName);
+                Application.Current.Properties.Add("DisplayName", user.Name);
+                Application.Current.Properties.Add("EmailAddress", user.Email);
+                Application.Current.Properties.Add("ProfilePicture", user.Picture);
+
+                await Navigation.PushAsync(new ProfilePage());
+
             }
         }
 
